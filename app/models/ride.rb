3bb82ea9c7f41d2fa8b2e0a4e4e909d9ab3ea631
morgancfarrow90 <63519344+morgancfarrow90@users.noreply.github.com>
2020-06-18 16:04:08 -0400

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,2 +1,5 @@
-class Ride < ActiveRecord::Base
+class Ride < ApplicationRecord
+  belongs_to :attraction
+  belongs_to :user
+  validates :user_id, :attraction_id, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
-class User < ActiveRecord::Base
+class User < ApplicationRecord
+  has_secure_password
+  has_many :rides
+  has_many :attractions, :through => :rides
+  validates :name, :password, :happiness, :nausea, :height, :tickets, :admin, presence: true
 end
