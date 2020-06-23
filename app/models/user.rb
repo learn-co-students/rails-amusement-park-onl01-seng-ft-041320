589efd3df9a0a1ede4d6b0fc9f46a4ab8d50561a
webdevjscoder <cobianjuan19@gmail.com>
2020-06-23 16:50:24 -0700

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
 class User < ActiveRecord::Base
+    has_secure_password
+    has_many :rides
+    has_many :attractions, through: :rides
+    def mood
+        if self.nausea.to_i < self.happiness.to_i
+            'happy'
+        else
+            'sad'
+        end
+    end
+
+    def is_admin?
+        return "ADMIN" if self.admin
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  root 'welcome#home'
+  resources :users
+  resources :attractions
+  resources :rides, only: [:new, :create]
+
+  get '/signin' => 'sessions#new'
+  post '/signin' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
