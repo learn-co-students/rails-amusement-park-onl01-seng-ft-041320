9bd6c79dae06a0c164af843af96a52d74400365e
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-06-25 23:20:14 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  
+  root 'welcome#home'
+  resources :rides
+  resources :users
+  resources :attractions
+  get '/signin', to: 'session#new', as: 'signin'
+  post '/session', to: 'session#create', as: 'session'
+  delete '/session/', to: 'session#destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
