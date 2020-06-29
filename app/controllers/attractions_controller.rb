e7f69776d1b6a60b058fcf4a1699865ecb9adfa7
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-06-28 20:16:15 -0400

    Done.

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -4,12 +4,29 @@ class AttractionsController < ApplicationController
         @attractions = Attraction.all
     end
 
+    def show
+        @attraction = Attraction.find_by(id: params[:id])
+        @ride = @attraction.rides.build(user_id: current_user.id)
+    end
+
     def new
         @attraction = Attraction.new
     end
 
-    def show
+    def create
+        @attraction = Attraction.create(attraction_params)
+        redirect_to attraction_path(@attraction)
+    end
+
+    def edit
         @attraction = Attraction.find_by(id: params[:id])
+        @ride = @attraction.rides.build(user_id: current_user.id)
+    end
+
+    def update
+        @attraction = Attraction.find_by(id: params[:id])
+        @attraction.update(attraction_params)
+        redirect_to attraction_path(@attraction)
     end
 
     private
